266f97a902d591b859ccbeeadcb055ca249da46f
slamkajs <jslamka5685@gmail.com>
2012-11-27 16:01:48 -0500

    test

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
-var express = require('express');
+var express = require('express'),
+	haibu = require('haibu'),
+  eyes = require('eyes');
 
-var app = express();
-
-app.set('views', __dirname + '/app/views');
-app.set('view engine', 'ejs');
-app.set("view options", { layout: __dirname +"/app/views/layouts/layout.ejs" });
-
-require('./config/routes')(app);
-
-app.on('listening',function(){
-    console.log('ok, server is running');
+var client = new haibu.drone.Client({
+	host: 'localhost',
+	port: process.env.PORT || 3000
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port);
+client.set('views', __dirname + '/app/views');
+client.set('view engine', 'ejs');
+client.set("view options", { layout: __dirname +"/app/views/layouts/layout.ejs" });
+
+require('./config/routes')(client);
+
+client.on('listening',function(){
+    console.log('ok, server is running');
+});z
+
+client.start(app, function(err, res) {
+  if(err) {
+    console.log('Error spawning app: ' + app.name);
+    return eyes.inspect(err);
+  }
+
+  console.log('Successfully spawned app: ' + app.name);
+  eyes.inspect(result);
+});
+
+client.start(app, function(err, result) {
+    eyes.inspect(err);
+    eyes.inspect(result);
+});
+
+
+// var port = process.env.PORT || 3000;
+// client.listen(port);

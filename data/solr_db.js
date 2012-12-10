b7d3267e672ef8877e32f386e12a9072df5e0616
slamkajs <jslamka5685@gmail.com>
2012-12-10 10:57:34 -0500

    Added proper IP address for solr server.

--- data/solr_db.js
+++ data/solr_db.js
@@ -1,7 +1,7 @@
 var solr = require('solr-client');
 
 var client = solr.createClient({
-	host: '127.0.0.1',
+	host: '10.168.1.55',
 	port: '8983',
 	core: 'activities',
 	path: '/solr'
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express')
-  , port = process.env.PORT || 3000
-  , http = require('http');
+  , port = process.env.PORT || 3001
+  , http = require('http')
+  , eyes = require('eyes')
 
 var app = express();
+
 
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'ejs');
@@ -10,8 +12,30 @@ app.set("view options", { layout: __dirname +"/app/views/layouts/layout.ejs" });
 
 require('./config/routes')(app);
 
+app.listen(port);
+
 app.on('listening',function(){
     console.log('ok, server is running');
 });
 
-http.createServer(app).listen(port);
+// // Create a new client for communicating with the haibu server
+// var client = new haibu.drone.Client({
+//   host: 'http://haibuhome.com/',
+//   port: 9002
+// });
+
+// // Attempt to start up a new application
+// client.start(settings, function (err, result) {
+//   if (err) {
+//     console.log('Error spawning app: ' + settings.name);
+//     return eyes.inspect(err);
+//   }
+
+//   console.log('Successfully spawned app:');
+//   eyes.inspect(result);
+// });
+
+// client.start(settings, function (err, result) {
+//   eyes.inspect(err);
+//   eyes.inspect(result);
+// });
